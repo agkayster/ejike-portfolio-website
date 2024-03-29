87a49ddf9f3c21d7f7400d741fdc2a1897e734ef
agkayster <ejike.chiboka@gmail.com>
2024-03-29 07:27:21 +0100

    Implemented Portfolio page

---
+++ src/components/project.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
+
+const ProjectComponent = ({ projectList, x }) => {
+	return (
+		/* PROJECT CONTAINER */
+		<div className='sticky top-0 flex h-screen gap-4 items-center overflow-hidden'>
+			<motion.div style={{ x }} className='flex'>
+				<div className='h-screen w-screen flex items-center justify-center bg-gradient-to-r from-purple-300 to-red-300' />
+				{projectList &&
+					projectList.map(({ id, color, title, desc, img, link }) => (
+						<div
+							key={id}
+							className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${color}`}>
+							<div className='flex flex-col gap-8 text-white'>
+								<h1 className='text-xl font-bold md:text-4xl lg:text-6xl xl:text-8xl'>
+									{title}
+								</h1>
+								<div className='relative w-80 h-56 md:w-96 md:h-64 lg:w-[500px]'>
+									<Image
+										src={img}
+										alt='images'
+										fill
+										className='absolute'
+									/>
+								</div>
+								{/* <div className='relative w-80 h-56 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px] xl:w-[600px] xl:h-[420px]'>
+												<Image
+													src={img}
+													alt='images'
+													fill
+													className='absolute'
+												/>
+											</div> */}
+								<p className='w-80 md:w-96 lg:w-[500px] xl:w-[600px] lg:text-lg'>
+									{desc}
+								</p>
+								<Link href={link} className='flex justify-end'>
+									<button
+										className='p-2 text-sm md:p-4 md:text-base lg:p-8 lg:text-lg bg-white
+												 text-gray-600 font-semibold m-4 rounded'>
+										See Demo
+									</button>
+								</Link>
+							</div>
+						</div>
+					))}
+			</motion.div>
+		</div>
+	);
+};
+
+export default ProjectComponent;
---
+++ src/components/projectContact.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
+
+const ProjectContComponent = () => {
+	return (
+		/* CONTACT FOR PROJECT */
+		<div className='h-screen w-screen flex flex-col gap-16 items-center justify-center text-center'>
+			<h1 className='text-3xl md:text-6xl'>Do you have a Project?</h1>
+			{/* ROTATING ANIMATION SVG */}
+			{/* ROTATING SVG CONTAINER */}
+			<div className='relative'>
+				<motion.svg
+					animate={{ rotate: 360 }}
+					transition={{
+						duration: 8,
+						ease: 'linear',
+						repeat: Infinity,
+					}}
+					viewBox='0 0 300 300'
+					className='w-64 h-64 md:w-[500px] md:h-[500px]'>
+					<defs>
+						<path
+							id='circlePath'
+							d='M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 '
+						/>
+					</defs>
+					<text fill='#000'>
+						<textPath xlinkHref='#circlePath' className='text-xl'>
+							Full-Stack Developer and AI Engineer
+						</textPath>
+					</text>
+				</motion.svg>
+				<Link
+					href='/contact'
+					className='w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 bg-black text-white rounded-full flex items-center justify-center m-auto cursor-pointer'>
+					Hire Me
+				</Link>
+			</div>
+		</div>
+	);
+};
+
+export default ProjectContComponent;

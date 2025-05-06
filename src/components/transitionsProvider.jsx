2de3c5993bfd50791ccc7d453362ae989eb1686e
agkayster <ejike.chibok@gmail.com>
2025-05-06 13:57:07 +0100

    removed transition text for pages

--- src/components/transitionsProvider.jsx
+++ src/components/transitionsProvider.jsx
@@ -25,8 +25,8 @@ const TransitionProvider = ({ children }) => {
 				{/* allows the page we are transiting to show in our animation */}
 				<motion.div
 					/* this centers the text inside */
-					className='fixed m-auto top-0 bottom-0 left-0 right-0 text-white text-8xl 
-          cursor-default z-50 w-fit h-fit'
+					// className='fixed m-auto top-0 bottom-0 left-0 right-0 text-white text-8xl
+					// cursor-default z-50 w-fit h-fit'
 					initial={{ opacity: 1 }}
 					animate={{ opacity: 0 }}
 					exit={{ opacity: 0 }}
